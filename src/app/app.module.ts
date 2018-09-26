38b72db7895ae11db03f0e9a320cc8fd5b83aed2
Justin Grimes <j_grimes@hotmail.com>
2018-09-26 11:45:12 -0400

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavTabsComponent } from './nav-tabs/nav-tabs.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: NavTabsComponent
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
@@ -15,7 +27,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
