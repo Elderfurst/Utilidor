3d3dada7392d17d025f29b39f8fa9552233e4775
Tim Wong <twong@reliaslearning.com>
2018-09-26 10:51:46 -0400

    Initial tabs added to main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavTabsComponent } from './nav-tabs/nav-tabs.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavTabsComponent
   ],
   imports: [
     BrowserModule,
